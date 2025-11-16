093402e8a5f5751b84549b0a9484c81103bddb1a
tkaliyev1993-a11y <tkaliyev1993@gmail.com>
2025-11-16 12:33:28 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-document.getElementById('leadForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-
-    let name = document.getElementById('name').value;
-    let phone = document.getElementById('phone').value;
-    let city = document.getElementById('city').value;
-    let service = document.getElementById('service').value;
-
-    let msg = `Заявка:%0AИмя: ${name}%0AТелефон: ${phone}%0AГород: ${city}%0AУслуга: ${service}`;
-    window.location.href = `https://wa.me/77052606667?text=${msg}`;
-});
+console.log('Sunkar Finance loaded');
